864aa6401552357376a2cec6632cec09856d5f7c
MarkRamos98 <marcos.ramos123@live.com>
2024-09-12 22:01:28 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
-MaterialButton btorecuperar,btocadastro,btoentrar;
+    MaterialButton btorecuperar, btocadastro, btoentrar;
+    TextInputEditText TxtEmail, TxtSenha;
 
 
     @Override
@@ -31,13 +35,33 @@ MaterialButton btorecuperar,btocadastro,btoentrar;
         btocadastro = findViewById(R.id.btocadastro);
         btorecuperar = findViewById(R.id.btorecuperar);
         btoentrar = findViewById(R.id.btoentrar);
+        TxtEmail = findViewById(R.id.TextEmail);
+        TxtSenha = findViewById(R.id.TextSenha);
 
         //criando ações para os botões
+
+        btoentrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, senha;
+
+                email = TxtEmail.getText().toString().trim();
+                senha = TxtSenha.getText().toString().trim();
+
+                if (email.equals('etecia') && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText().show();
+                }
+            }
+        });
+
         btocadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(getApplicationContext(),CadastrarActivity.class));
+                startActivity(new Intent(getApplicationContext(), CadastrarActivity.class));
                 finish();
 
             }
