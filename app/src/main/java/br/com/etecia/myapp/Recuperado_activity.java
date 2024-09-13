9223123097d114ab97eeebf74c09f22121cc721f
MarkRamos98 <marcos.ramos123@live.com>
2024-09-12 21:12:35 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/Recuperado_activity.java
+++ app/src/main/java/br/com/etecia/myapp/Recuperado_activity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Recuperado_activity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
 
+public class Recuperado_activity extends AppCompatActivity {
+MaterialToolbar recretorno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,15 @@ public class Recuperado_activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recretorno=findViewById(R.id.recretorno);
+        recretorno.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        });
+
     }
 }
