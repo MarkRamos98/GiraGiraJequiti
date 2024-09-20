c3503c277148c0cfc99d8d071ac879bc66537668
MarkRamos98 <marcos.ramos123@live.com>
2024-09-19 21:44:25 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuActivity.java
@@ -1,15 +1,17 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuActivity extends AppCompatActivity {
-
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+CardView bike, acessorios, manutencao, locais, dicas, eventos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,16 @@ public class MenuActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bike = findViewById(R.id.menubicycle);
+        acessorios = findViewById(R.id.menugadgets);
+        manutencao = findViewById(R.id.menumanutencao);
+        locais = findViewById(R.id.menulocais);
+        dicas = findViewById(R.id.menudicas);
+        eventos = findViewById(R.id.menuevent);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
