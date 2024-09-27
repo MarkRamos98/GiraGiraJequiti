7054e536c6daa9be75f47363880e4f6e14af51b6
MarkRamos98 <marcos.ramos123@live.com>
2024-09-26 21:20:10 -0300

    Fraguimentação

--- app/src/main/java/br/com/etecia/myapp/BikesUsadasFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesUsadasFragment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BikesNovasFragment extends Fragment {
+public class BikesUsadasFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+        View view = inflater.inflate(R.layout.fragment_bikes_usadas, container, false);
 
         return view;
     }
