cd0722886eb45601ccc14ef997c52ee4b9f2ce3b
MarkRamos98 <marcos.ramos123@live.com>
2024-10-03 20:24:17 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/Bike_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Bike_Activity.java
@@ -1,14 +1,20 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Bike_Activity extends AppCompatActivity {
+
+    TabLayout idTabLayoutBikes;
+    ViewPager2 ViewPagerBikes;
+    AdapterViewPagerBikes adapterViewPagerBikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,49 @@ public class Bike_Activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        idTabLayoutBikes = findViewById(R.id.idTabLayoutBikes);
+        ViewPagerBikes = findViewById(R.id.ViewPagerBikes);
+
+        adapterViewPagerBikes = new AdapterViewPagerBikes(this);
+
+        ViewPagerBikes.setAdapter(adapterViewPagerBikes);
+
+        idTabLayoutBikes.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+
+
+            }
+        });
+
+        ViewPagerBikes.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+
+                idTabLayoutBikes.getTabAt(position);
+
+            }
+        });
+
+
+
     }
+
 }
