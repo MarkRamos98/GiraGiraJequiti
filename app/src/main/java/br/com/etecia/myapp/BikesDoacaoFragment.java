4a452086f127bb1865e31732b4c40f8ce067cee5
MarkRamos98 <marcos.ramos123@live.com>
2024-10-10 20:17:54 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/BikesDoacaoFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesDoacaoFragment.java
@@ -7,14 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BikesDoacaoFragment extends Fragment {
 
+    FloatingActionButton IdFABBikesDoacoes;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bikes_doacao, container, false);
+
+        IdFABBikesDoacoes = view.findViewById(R.id.IdFABBikesDoacoes);
+
+        IdFABBikesDoacoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Cliquei no FAB", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
