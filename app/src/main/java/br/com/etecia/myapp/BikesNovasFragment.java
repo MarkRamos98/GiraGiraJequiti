cc447fc5d5abeef7e3b4c33004e223d54f96ec54
MarkRamos98 <marcos.ramos123@live.com>
2024-10-17 21:31:05 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -28,16 +29,23 @@ public class BikesNovasFragment extends Fragment {
         //carregando os objetos na lista
         lstBikenova = new ArrayList<>();
         lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Sportmarelo",R.drawable.ImagemA));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
-        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Sportmarelo",R.drawable.imagema));
+        lstBikenova.add(new Bikenova("CasuVerme",R.drawable.imagemb));
+        lstBikenova.add(new Bikenova("Sportblack",R.drawable.imagemc));
+        lstBikenova.add(new Bikenova("SportBranco",R.drawable.imagemd));
+        lstBikenova.add(new Bikenova("YingYang",R.drawable.imageme));
+        lstBikenova.add(new Bikenova("SportCult",R.drawable.imagemf));
+        lstBikenova.add(new Bikenova("Monster",R.drawable.imagemg));
+        lstBikenova.add(new Bikenova("Pantera",R.drawable.imagemh));
+        lstBikenova.add(new Bikenova("Wine",R.drawable.imagemi));
+        lstBikenova.add(new Bikenova("Street",R.drawable.imagemj));
+
+        //carregando o adaptador
+        RecyclerAdapterBikeNova adapterBikeNova = new RecyclerAdapterBikeNova(getContext(),lstBikenova);
+
+        idrecbikesnovas.setLayoutManager(new GridLayoutManager(getContext(),2));
+
+        idrecbikesnovas.setAdapter(adapterBikeNova);
 
         return view;
     }
