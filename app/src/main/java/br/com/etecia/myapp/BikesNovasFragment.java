12f02aedb0ad32908493e89b4503f48c387ff9ce
MarkRamos98 <marcos.ramos123@live.com>
2024-10-17 21:11:42 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
@@ -3,18 +3,41 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BikesNovasFragment extends Fragment {
+
+    private List<Bikenova> lstBikenova;
+    RecyclerView idrecbikesnovas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+
+        idrecbikesnovas = view.findViewById(R.id.idrecbikesnovas);
+
+        //carregando os objetos na lista
+        lstBikenova = new ArrayList<>();
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Sportmarelo",R.drawable.ImagemA));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
+        lstBikenova.add(new Bikenova("Batman",R.drawable.batma));
 
         return view;
     }
